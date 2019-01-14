d6a58b264cd940a3e077e643fdcafa528bccebec
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-13 20:10:08 -0500

    Updated Parameterized Consumer

--- src/main/java/com/springtesting/security/config/SecurityConfig.java
+++ src/main/java/com/springtesting/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.springtesting.security.config;
 
 
 import com.springtesting.security.MyUserDetailsService;
+import com.springtesting.security.filters.CustomBasicAuthenticationFilter;
 import com.springtesting.security.filters.CustomFilter;
 import com.springtesting.security.handlers.CustomAuthenticationFailureHandler;
 import com.springtesting.security.handlers.CustomAuthenticationSuccessHandler;
@@ -69,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception
     {
         http.addFilterBefore(new CustomFilter(), BasicAuthenticationFilter.class)
-
+                .addFilter(new CustomBasicAuthenticationFilter(authenticationManagerBean()))
                 .authorizeRequests()
                     .antMatchers("/anonymous*").anonymous()
                     //.antMatchers("/users/**").permitAll()
