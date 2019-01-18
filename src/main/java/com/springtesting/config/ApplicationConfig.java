cb5afe58552da3acf433a3351224a659ba8d5979
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-18 16:58:30 -0500

    Updated

--- src/main/java/com/springtesting/config/ApplicationConfig.java
+++ src/main/java/com/springtesting/config/ApplicationConfig.java
@@ -10,18 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer
 {
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
-            {
-                "classpath:/META-INF/resources/", "classpath:/resources/",
-                "classpath:/static/", "classpath:/public/","classpath:/static/vendor/","classpath:/static/custom/"
-            };
-
+    /*private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
+    {
+        "classpath:/META-INF/resources/", "classpath:/resources/",
+        "classpath:/static/", "classpath:/public/","classpath:/static/vendor/","classpath:/static/custom/"
+    };
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
     {
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }
+    }*/
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder()
