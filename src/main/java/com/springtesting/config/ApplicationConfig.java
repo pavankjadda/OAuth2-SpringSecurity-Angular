2ba1db1650b4245d9ba71f87f4f9ecaebe2ad4c4
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-18 16:29:21 -0500

    Updated

--- src/main/java/com/springtesting/config/ApplicationConfig.java
+++ src/main/java/com/springtesting/config/ApplicationConfig.java
@@ -10,9 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer
 {
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/","classpath:/static/vendor/","classpath:/static/custom/" };
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
+            {
+                "classpath:/META-INF/resources/", "classpath:/resources/",
+                "classpath:/static/", "classpath:/public/","classpath:/static/vendor/","classpath:/static/custom/"
+            };
 
 
     @Override
