c24c93f0b5bd5f27dba6ddbda330036fdf9aeb7e
Pavan Jadda <jpavanaryan@gmail.com>
2019-02-14 18:00:40 -0500

    Updated package name

--- src/main/java/com/springsessiondemo/config/ApplicationConfig.java
+++ src/main/java/com/springsessiondemo/config/ApplicationConfig.java
@@ -1,15 +1,32 @@
 package com.springsessiondemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer
 {
+
+    @Value("classpath:oauth_schema.sql")
+    private Resource schemaScript;
+
+    @Autowired
+    public Environment env;
+
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
     {
         "classpath:/META-INF/resources/", "classpath:/resources/",
@@ -34,10 +51,32 @@ public class ApplicationConfig implements WebMvcConfigurer
         return new HttpSessionEventPublisher();
     }
 
-    /*@Bean
-    SpringSessionBackedSessionRegistry springSessionBackedSessionRegistry()
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource)
     {
-        return (SpringSessionBackedSessionRegistry) new ParameterizedConsumer<>().getSessionRepository();
-    }*/
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator()
+    {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schemaScript);
+        return populator;
+    }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getProperty("jdbc.url"));
+        dataSource.setUsername(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.pass"));
+        return dataSource;
+    }
 
 }
---
+++ src/main/java/com/springsessiondemo/security/config/AuthServerOAuth2Config.java
@@ -0,0 +1,64 @@
+package com.springsessiondemo.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
+{
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception
+    {
+        oauthServer.tokenKeyAccess("permitAll()")
+                    .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception
+    {
+        clients.jdbc(dataSource)
+                .withClient("sampleClientId")
+                    .authorizedGrantTypes("implicit")
+                    .scopes("read")
+                    .autoApprove(true)
+                .and()
+                    .withClient("clientIdPassword")
+                    .secret("secret")
+                .authorizedGrantTypes("password","authorization_code", "refresh_token")
+                .scopes("read");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception
+    {
+        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    public TokenStore tokenStore()
+    {
+        return new JdbcTokenStore(dataSource);
+    }
+
+
+}
