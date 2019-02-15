fa6accb54344eb11f601428e18ea61ca1e6d085e
Pavan Jadda <jpavanaryan@gmail.com>
2019-02-15 14:03:22 -0500

    OAuth Working

--- src/main/java/com/springsessiondemo/security/config/OAuth2ResourceServerConfig.java
+++ src/main/java/com/springsessiondemo/security/config/OAuth2ResourceServerConfig.java
@@ -1,7 +1,6 @@
 package com.springsessiondemo.security.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -12,17 +11,10 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter
 {
     private static final String RESOURCE_ID = "resource-server-rest-api";
 
-
     @Override
     public void configure(ResourceServerSecurityConfigurer resources)
     {
         resources.resourceId(RESOURCE_ID);
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception
-    {
-        http.authorizeRequests()
-                .antMatchers("/api/v2/category").authenticated();
-    }
 }
--- src/main/java/com/springsessiondemo/security/config/SecurityConfig.java
+++ src/main/java/com/springsessiondemo/security/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -44,6 +45,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     {
         this.userDetailsService = userDetailsService;
         this.userPasswordEncoder = userPasswordEncoder;
+    }
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+                .antMatchers("/api/v2/category").authenticated();
     }
 
     @Override
