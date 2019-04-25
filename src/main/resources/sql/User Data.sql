31cc76c50cf0cb1cec56f8df0627c2770072d7a3
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-24 22:55:00 -0400

    Fixed a bug where POST and PUT requests are not accepted

--- src/main/resources/sql/User Data.sql
+++ src/main/resources/sql/User Data.sql
@@ -1,27 +1,5 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-/* Insert Data into Category Table */
-INSERT INTO springsessiondemo.category VALUES(1002,'Grocery');
-INSERT INTO springsessiondemo.category VALUES(1003,'Electronics');
-INSERT INTO springsessiondemo.category VALUES(1001,'Books');
-
-
-/* Insert Data into Product Table  */
-INSERT INTO springsessiondemo.product VALUES(2001,'iPhoneX',1003);
-INSERT INTO springsessiondemo.product VALUES(2002,'GalaxyS10',1003);
-INSERT INTO springsessiondemo.product VALUES(2003,'Google Pixel',1003);
-
-/* Insert Data into Order Detail Table  */
-INSERT INTO springsessiondemo.order_detail VALUES(1001,1001);
-
-/* Insert Data into Order Detail Products Table  */
-INSERT INTO springsessiondemo.order_detail_productlist VALUES(1,'ord1001',2001);
-INSERT INTO springsessiondemo.order_detail_productlist VALUES(2,'ord1001',2002);
-INSERT INTO springsessiondemo.order_detail_productlist VALUES(3,'ord1001',2003);
-
-
-
-
 
 /* Insert Data into User Table  */
 INSERT INTO springsessiondemo.user VALUES(1,1,1,1,1,'$2a$12$pqcdXB.Xboa7pGNba51YHuWQVhlZM8TVguRUCL2ss8GOwhEqiqwOu','admin');
