7e72920daf803e047a2df8f734b8bb7f0bb1dc2f
Pavan Jadda <jpavanaryan@gmail.com>
2019-07-09 01:32:36 -0400

    Update Routes and remove unused components

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
-# Spring Security implementation with Spring Session, Spring Data and Spring Boot
+# OAuth2 authentication with Spring Boot 2, Angular 8 and MySql
 
 Spring Security implementation with Spring Session, Spring Data and Spring Boot. Following usecases covered
 
-#### Requirments
-1. SpringBoot (IntellijJ or Eclipse will be downloaded required dependencies after import)
-2. MySql (create database named `springsessiondemo`)
-3. Execute sql queries from [this page](https://github.com/pavankjadda/SpringSessionDemo/wiki/Create-OAuth-Tables) to create oauth tables (mandatory). These tables hold OAuth2 tokens
-4. Insert following records in oauth_client_details table and make sure to change `CLIENT_SECRET` value. **Compute this value with BCrypt (4 times hash) online and replace it with hashed value**
-```
-INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
- VALUES ('spring-security-oauth2-read-client', 'resource-server-rest-api',
- '/*spring-security-oauth2-read-client-password1234*/',
- 'read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
- 
-INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
- VALUES ('spring-security-oauth2-read-write-client', 'resource-server-rest-api',
- '/*spring-security-oauth2-read-write-client-password1234*/',
- 'read,write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
- ```
-5.Run the project and Spring Data JPA creates all the tables necessary for Users and Roles
-6.Insert Roles and Users data mentioned in this [page](https://github.com/pavankjadda/SpringSessionDemo/wiki/Users-and-Roles)
-
-
+#### How to Run?
+Please follow instructions from https://medium.com/@pavankjadda/oauth2-authentication-with-spring-boot2-mysql-and-angular-7-488bdfe82cc1 
--- src/main/resources/sql/Insert OAuth clients, User and Category Data.sql
+++ src/main/resources/sql/Insert OAuth clients, User and Category Data.sql
@@ -4,36 +4,36 @@ INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, 
 
 
 /* Insert Data into User Table  */
-INSERT INTO springsessiontest.user VALUES(1,1,1,1,1,'$2a$12$xTnx9IuV4Kb2OZav540BReKnqgpIYYgHce4ZJb2QTxTQ6j7H5NLlS','admin');
-INSERT INTO springsessiontest.user VALUES(2,1,1,1,1,'$2a$12$kRstkVGM88bvzGqxcWDX5eVDvT0iy40YPAKMRiKX9D2jIsXmKXOAK','user');
+INSERT INTO springoauthdemo.user VALUES(1,1,1,1,1,'$2a$12$xTnx9IuV4Kb2OZav540BReKnqgpIYYgHce4ZJb2QTxTQ6j7H5NLlS','admin');
+INSERT INTO springoauthdemo.user VALUES(2,1,1,1,1,'$2a$12$kRstkVGM88bvzGqxcWDX5eVDvT0iy40YPAKMRiKX9D2jIsXmKXOAK','user');
 
 
 /* Insert Data into Role Table  */
-insert into springsessiontest.role values(1,'ROLE_USER');
-insert into springsessiontest.role values(2,'ROLE_ADMIN');
-insert into springsessiontest.role values(3,'ROLE_APIUSER');
-insert into springsessiontest.role values(4,'ROLE_DBA');
-insert into springsessiontest.role values(5,'ROLE_SELLER');
-insert into springsessiontest.role values(6,'ROLE_BUYER');
+insert into springoauthdemo.role values(1,'ROLE_USER');
+insert into springoauthdemo.role values(2,'ROLE_ADMIN');
+insert into springoauthdemo.role values(3,'ROLE_APIUSER');
+insert into springoauthdemo.role values(4,'ROLE_DBA');
+insert into springoauthdemo.role values(5,'ROLE_SELLER');
+insert into springoauthdemo.role values(6,'ROLE_BUYER');
 
 
 /* Insert Data into Privilege Table  */
-insert into springsessiontest.privilege values(1,'READ_PRIVILEGE');
-insert into springsessiontest.privilege values(2,'WRITE_PRIVILEGE');
-insert into springsessiontest.privilege values(3,'DELETE_PRIVILEGE');
+insert into springoauthdemo.privilege values(1,'READ_PRIVILEGE');
+insert into springoauthdemo.privilege values(2,'WRITE_PRIVILEGE');
+insert into springoauthdemo.privilege values(3,'DELETE_PRIVILEGE');
 
 /* Insert Data into UserRole Table  */
-INSERT INTO `springsessiontest`.`user_role`(`id`,`user_id`,`role_id`) VALUES (1,2,1);
-INSERT INTO `springsessiontest`.`user_role`(`id`,`user_id`,`role_id`) VALUES (2,1,2);
+INSERT INTO `springoauthdemo`.`user_role`(`id`,`user_id`,`role_id`) VALUES (1,2,1);
+INSERT INTO `springoauthdemo`.`user_role`(`id`,`user_id`,`role_id`) VALUES (2,1,2);
 
 /* Insert Data into RolePrivilege Table  */
-insert into springsessiontest.role_privilege values(2,1);
-insert into springsessiontest.role_privilege values(2,2);
-insert into springsessiontest.role_privilege values(2,3);
-insert into springsessiontest.role_privilege values(1,1);
+insert into springoauthdemo.role_privilege values(2,1);
+insert into springoauthdemo.role_privilege values(2,2);
+insert into springoauthdemo.role_privilege values(2,3);
+insert into springoauthdemo.role_privilege values(1,1);
 
 
 /* Insert Data into Category Table */
-INSERT INTO springsessiontest.category VALUES(1002,'Grocery');
-INSERT INTO springsessiontest.category VALUES(1003,'Electronics');
-INSERT INTO springsessiontest.category VALUES(1001,'Books');
+INSERT INTO springoauthdemo.category VALUES(1002,'Grocery');
+INSERT INTO springoauthdemo.category VALUES(1003,'Electronics');
+INSERT INTO springoauthdemo.category VALUES(1001,'Books');
